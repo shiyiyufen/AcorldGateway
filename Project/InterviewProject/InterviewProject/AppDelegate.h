daca94a067c8a71ce397c79c734f7ad9d542c7de
何欣宇 <cdhexinyu@jd.com>
2014-09-15 18:04:51 +0800

    iOS使用多线程提高数据并发访问

---
+++ Project/InterviewProject/InterviewProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InterviewProject
+//
+//  Created by JD_Acorld on 14-9-13.
+//  Copyright (c) 2014年 hxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
